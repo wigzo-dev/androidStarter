894115f5cc591b6d8a54d162f9606b5cc6cf10cd
Rihan Husain <rihanaster@gmail.com>
2017-02-16 12:07:02 +0530

    Sending Notification title, body and intent Data to the user so that data can be used by him accordingly. Added relevant code in MyFibaseMessagingService for demo purpose. Also added WigzoBaseActivity to be extended by user in every activity he creates in order to track app running status

--- WigzoPushSampleNoFCM/app/src/main/java/wigzo/com/wigzopushsamplenofcm/MainActivity.java
+++ WigzoPushSampleNoFCM/app/src/main/java/wigzo/com/wigzopushsamplenofcm/MainActivity.java
@@ -2,16 +2,21 @@ package wigzo.com.wigzopushsamplenofcm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.wigzo.sdk.AbstractWigzoFcmListenerService;
+import com.wigzo.sdk.WigzoBaseActiviy;
 import com.wigzo.sdk.WigzoInstanceIDService;
 import com.wigzo.sdk.WigzoSDK;
+import com.wigzo.sdk.helpers.Configuration;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends WigzoBaseActiviy {
 
 
     private String ORG_TOKEN = "";
@@ -27,20 +32,22 @@ public class MainActivity extends AppCompatActivity {
         showNotification = (Button) findViewById(R.id.showNotification);
         showDemandedText = (TextView) findViewById(R.id.showDemandedText);
 
-        ORG_TOKEN = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE).getString("ORG_TOKEN", "56065c5b-db30-4b89-bd76-0a9c2938c90b");
-        WigzoSDK.getInstance().initializeWigzoData(getApplicationContext(), ORG_TOKEN);
+        ORG_TOKEN = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE).getString("ORG_TOKEN", "09d8699d-aead-4bf9-a6b2-c606181956de");
+        WigzoSDK.getInstance().initializeWigzoData(MainActivity.this, ORG_TOKEN);
 
         showToken.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDemandedText.setText(WigzoInstanceIDService.refreshedToken);
+                //showDemandedText.setText(WigzoInstanceIDService.refreshedToken);
+                showDemandedText.setText(FirebaseInstanceId.getInstance().getToken());
+                Log.e("new Token", FirebaseInstanceId.getInstance().getToken());
             }
         });
 
         showNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDemandedText.setText(AbstractWigzoFcmListenerService.msg.getNotification().getBody());
+                showDemandedText.setText(AbstractWigzoFcmListenerService.msg.getData().toString());
             }
         });
     }
--- WigzoPushSampleNoFCM/app/src/main/java/wigzo/com/wigzopushsamplenofcm/MyFirebaseMessagingService.java
+++ WigzoPushSampleNoFCM/app/src/main/java/wigzo/com/wigzopushsamplenofcm/MyFirebaseMessagingService.java
@@ -1,8 +1,11 @@
 package wigzo.com.wigzopushsamplenofcm;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.wigzo.sdk.AbstractWigzoFcmListenerService;
+
+import java.util.HashMap;
 
 /**
  * Created by wigzo on 30/11/16.
@@ -11,6 +14,10 @@ import com.wigzo.sdk.AbstractWigzoFcmListenerService;
 public class MyFirebaseMessagingService extends AbstractWigzoFcmListenerService {
     @Override
     protected Class<? extends Activity> getTargetActivity() {
-        return null;
+
+        HashMap<String, String> payload = getWigzoNotificationPayload();
+        Log.e("FCM", "Title: " + getWigzoNotificationTitle() + " Body: " + getWigzoNotificationBody());
+        Log.e("FCM", "Payload: " + payload);
+        return MainActivity.class;
     }
 }
